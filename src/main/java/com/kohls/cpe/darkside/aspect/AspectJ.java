e16a459d8a246c3f414fd00c25a2be27be2954cd
Timofei Barabanov <t.barabanov@gmail.com>
2021-11-18 15:40:38 +0300

    around advice

--- src/main/java/com/kohls/cpe/darkside/aspect/AspectJ.java
+++ src/main/java/com/kohls/cpe/darkside/aspect/AspectJ.java
@@ -1,7 +1,9 @@
 package com.kohls.cpe.darkside.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.context.annotation.Configuration;
@@ -12,14 +14,19 @@ import java.util.Arrays;
 @Aspect
 public class AspectJ {
 
+
     @Before("@within(org.springframework.web.bind.annotation.RestController) && args(firstName,..)")
-    public void beforeAction(JoinPoint joinPoint, String firstName) {
+    public void beforeRestAction(JoinPoint joinPoint, String firstName) {
         System.out.println("here I am! " + Arrays.toString(joinPoint.getArgs()));
     }
 
+    @Around("@within(org.springframework.web.bind.annotation.RestController)")
+    public Object aroundRestAction(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        return proceedingJoinPoint.proceed(proceedingJoinPoint.getArgs());
+    }
 
     @After("@within(org.springframework.web.bind.annotation.RestController)")
-    public void afterAction(JoinPoint joinPoint) {
+    public void afterRestAction(JoinPoint joinPoint) {
         System.out.println("here I am! " + Arrays.toString(joinPoint.getArgs()));
     }
 
